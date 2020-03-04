885d3acb5a94f996d939505643767296d88ff215
kmacosta2 <kmacosta2@miners.utep.edu>
2020-03-04 12:19:36 -0700

    //..

--- src/sortOfSort.java
+++ src/sortOfSort.java
@@ -47,6 +47,7 @@ public class sortOfSort {
             }
         }
     }
+    //
     public void twoDprinter(int[] a){
             for (int i = 0; i < a.length; i++) {
                 System.out.print(a[i] + " ");
